73f132d45e99553865fdb304cf8db8f011778885
Frederik <frederik.bolding@gmail.com>
2016-03-26 04:45:50 +0100

    Fixed deadlock issues

--- OMDbSharp/OMDbClient.cs
+++ OMDbSharp/OMDbClient.cs
@@ -27,10 +27,10 @@ namespace OmdbSharp
                 client.DefaultRequestHeaders.Accept.Clear();
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-                HttpResponseMessage response = await client.GetAsync(omdbUrl + query + "&tomatoes=" + rottenTomatoesRatings);
+                HttpResponseMessage response = await client.GetAsync(omdbUrl + query + "&tomatoes=" + rottenTomatoesRatings).ConfigureAwait(false);
                 if (response.IsSuccessStatusCode)
                 {
-                    T obj = await response.Content.ReadAsAsync<T>();
+                    T obj = await response.Content.ReadAsAsync<T>().ConfigureAwait(false);
                     return obj;
                 }
                 else
@@ -42,31 +42,31 @@ namespace OmdbSharp
 
         public async Task<Item> GetItemByTitle(string title)
         {
-            Item item = await Request<Item>("t=" + title);
+            Item item = await Request<Item>("t=" + title).ConfigureAwait(false);
             return item;
         }
 
         public async Task<Item> GetItemByID(string id)
         {
-            Item item = await Request<Item>("i=" + id);
+            Item item = await Request<Item>("i=" + id).ConfigureAwait(false);
             return item;
         }
 
         public async Task<ItemList> GetItemList(string query)
         {
-            ItemList itemList = await Request<ItemList>("s=" + query);
+            ItemList itemList = await Request<ItemList>("s=" + query).ConfigureAwait(false);
             return itemList;
         }
 
         public async Task<Season> GetSeriesSeason(string id, int season)
         {
-            Season _season = await Request<Season>("i=" + id + "&Season=" + season);
+            Season _season = await Request<Season>("i=" + id + "&Season=" + season).ConfigureAwait(false);
             return _season;
         }
 
         public async Task<Episode> GetSeriesEpisode(string id, int season, int episode)
         {
-            Episode _episode = await Request<Episode>("i=" + id + "&Season=" + season + "&Episode=" + episode);
+            Episode _episode = await Request<Episode>("i=" + id + "&Season=" + season + "&Episode=" + episode).ConfigureAwait(false);
             return _episode;
         }
     }
